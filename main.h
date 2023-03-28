227dbfe0ccad1484b08c280b538839043aa58b03
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-03-28 11:42:03 -0700

    add 0-printf.c file and main.h

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef _PRINTF_EX_H
+#define _PRINTF_EX_H
+#include <unistd.h>
+#include <stdarg.h>
+int reverse_number(int);
+void print_int(int);
+void print_str(char *s);
+void print_spec_char(char);
+#endif
